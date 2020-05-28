02b08cd101db3ef13f311603600c29751a43922d
khaled romdhane <khaled@medgo.fr>
2020-05-29 00:30:08 +0200

    adding verify

--- src/middlewares/security/auth.middlewares.ts
+++ src/middlewares/security/auth.middlewares.ts
@@ -3,7 +3,7 @@ import * as jwt from "jsonwebtoken";
 
 export default async (req, res, next) => {
   if (req.token) {
-    const decoded = jwt.decode(req.token);
+    const decoded = jwt.verify(req.token, process.env.JWT_SECRET);
     if (!decoded) return res.status(401).send();
     req.user = await User.findOne({ _id: decoded._id });
     if (!req.user) {
